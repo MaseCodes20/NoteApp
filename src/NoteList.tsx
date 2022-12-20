aa4c83f199b685a074f2f43de89ca7ce2e8b1e8e
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-12-19 19:43:30 -0500

    Code Refactored: EditTagModal

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -13,6 +13,7 @@ import ReactSelect from "react-select";
 import { useMemo, useState } from "react";
 import { EditTagsModalProps, SimplifiedNote, Tag } from "./types/types";
 import NoteCard from "./NoteCard";
+import EditTagsModal from "./EditTagModal";
 // import NoteCard from "./NoteCard";
 
 type NoteListProps = {
@@ -123,44 +124,3 @@ function NoteList({
 }
 
 export default NoteList;
-
-function EditTagsModal({
-  availableTags,
-  show,
-  handleClose,
-  onUpdateTag,
-  onDeleteTag,
-}: EditTagsModalProps) {
-  return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Edit Tags</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Stack gap={2}>
-            {availableTags.map((tag) => (
-              <Row key={tag.id}>
-                <Col>
-                  <Form.Control
-                    type="text"
-                    value={tag.label}
-                    onChange={(e) => onUpdateTag(tag.id, e.target.value)}
-                  />
-                </Col>
-                <Col xs="auto">
-                  <Button
-                    onClick={() => onDeleteTag(tag.id)}
-                    variant="outline-danger"
-                  >
-                    &times;
-                  </Button>
-                </Col>
-              </Row>
-            ))}
-          </Stack>
-        </Form>
-      </Modal.Body>
-    </Modal>
-  );
-}
